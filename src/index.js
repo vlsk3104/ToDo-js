416a1dee469811ac2a179587462708a1b144a309
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-19 11:21:43 +0900

    タスクの戻す機能

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  cereateIncompleteList(inputText);
+};
+
+//未完了リストに追加する関数
+const cereateIncompleteList = (text) => {
   //divを生成
   const div = document.createElement("div");
   div.className = "list-row";
 
   //liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   //button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -36,11 +41,19 @@ const onClickAdd = () => {
     //buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //押された戻すボタンの親タグ（div）を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      //テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      cereateIncompleteList(text);
+    });
 
     //divタグの子要素に各要素を設定
     addTarget.appendChild(li);
     addTarget.appendChild(backButton);
-    console.log(addTarget);
 
     //完了リストに追加
     document.getElementById("complete-list").appendChild(addTarget);
